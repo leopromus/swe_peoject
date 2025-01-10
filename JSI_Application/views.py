f428e2f6c74de1e9c71b465098b12f4a9e158d9a
leopromus <leopromus@gmail.com>
2025-01-10 18:28:09 +0200

    remove files

--- JSI_Application/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-
-def Login(request):
-    return render(request,'Login.html')
-
-def Home(request):
-    return render(request,'Home.html')
--- README.md
+++
@@ -1,2 +0,0 @@
-# swe_peoject
-swe end of semester project on 20th december
